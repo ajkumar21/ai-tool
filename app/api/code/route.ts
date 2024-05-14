a0f9e4a5346cea1021b55da6789aacc76a7abe7d
AJ Kumar <ajanthan29894@gmail.com>
2024-05-14 01:14:40 +0100

    added code gen

---
+++ app/api/code/route.ts
@@ -0,0 +1,30 @@
+import { auth } from "@clerk/nextjs/server"
+import { NextResponse } from "next/server"
+import OpenAI from "openai"
+import { ChatCompletionMessageParam } from "openai/resources/index.mjs";
+
+const openai = new OpenAI()
+
+const instructionMessage: ChatCompletionMessageParam  = {
+    role: "system",
+    content:"You are a code generator. You must answer only in markdown code snippets. Use code comments for explanations."
+}
+
+export async function POST(req: Request) {
+    try {
+        const { userId } = auth()
+        const body = await req.json()
+        const { messages } = body
+
+        if (!userId) return new NextResponse("Unauthorized", { status: 401})
+        if (!openai) return new NextResponse("Open AI config failed", { status: 500})
+        if (!messages) return new NextResponse("Messages are required", {status: 400})
+        
+        const response = await openai.chat.completions.create({ model: "gpt-3.5-turbo", messages: [instructionMessage, ...messages]});
+        return NextResponse.json(response.choices[0].message);
+
+    }catch (err) {
+        console.log(`Code error: ${err}`)
+        return new NextResponse("Internal error", {status: 500})
+    }
+}
