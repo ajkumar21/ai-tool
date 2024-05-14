460b328ff53b0c26cd4f94c9304e2871247bbe50
AJ Kumar <ajanthan29894@gmail.com>
2024-05-14 16:10:30 +0100

    added music generation via replicate ai

---
+++ app/api/music/route.ts
@@ -0,0 +1,27 @@
+import { auth } from "@clerk/nextjs/server"
+import { NextResponse } from "next/server"
+import Replicate from "replicate"
+
+const replicate = new Replicate({
+    auth: process.env.REPLICATE_API_TOKEN
+})
+
+export async function POST(req: Request) {
+    try {
+        const { userId } = auth()
+        const body = await req.json()
+        const { prompt: prompt_a } = body
+
+        if (!userId) return new NextResponse("Unauthorized", { status: 401})
+        if (!prompt_a) return new NextResponse("Music prompt is required", {status: 400})
+
+        const response = await replicate.run("riffusion/riffusion:8cf61ea6c56afd61d8f5b9ffd14d7c216c0a93844ce2d82ac1c9ecc9c7f24e05", { input: {prompt_a} });
+
+        
+        return NextResponse.json(response);
+
+    }catch (err) {
+        console.log(`Conversation error: ${err}`)
+        return new NextResponse("Internal error", {status: 500})
+    }
+}
